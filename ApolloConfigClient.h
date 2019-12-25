4c304946005c76d5a6b00afbeda3b9d848510eee
NashWong <791745839@qq.com>
2019-12-25 14:59:06 +0800

    Update ApolloConfigClient.h
    阿波罗客户端更新

--- ApolloConfigClient.h
+++ ApolloConfigClient.h
@@ -4,15 +4,18 @@
 	octans下面有一个Apollo客户端的实现，但实现过于复杂，而且有点问题
 	1: 阿波罗的namespace 创建的时候可以根据配置文件的类型选择不同的类型 json xml等等
 	好处是在修改的时候会做相应的校验，保证配置的正确，如果是默认的properties 即使配置文件写错了，也不会有报错，挺严重
-	而且设置了类型后 就用再额外设置key namespace就是key 
+	而且设置了类型后 不用再额外设置key namespace就是key 
 	2: 阿波罗的接口如果配置没变化 1分钟后会返回304 有配置200 这个需要判断下，解析失败和http报错目前都会不停的调用
 	不太好
 	3: 用起来麻烦，原则上，只需要暴露出一个接口，接收string参数，然后解析出需要的内容就可以了
 	4: 不支持多个appid cluster的情况
-	4: 写的太复杂
+	5: 如果配置太多，异步获取、解析耗时，可能进程起来了 但是配置还没有加载到内存里，容易出现bug
+	6: 写的太复杂
 */
-#define _CRT_SECURE_NO_WARNINGS
+/*
+windows下用boost1.60的版本要有这个宏
 #define  _WIN32_WINNT  0x0600
+*/
 #include "LSingleton.h"
 #include<string>
 #include<memory>
@@ -25,22 +28,39 @@
 #include<unordered_map>
 #include<chrono>
 #include<thread>
-#include "client_http.hpp"
+#include "simplewebserver/client_http.hpp"
 #include "json.hpp"
-#define SLOG_ERROR(...)  std::cerr<<__VA_ARGS__
+#include "LLog.h"
+#include<atomic>
 /* 阿波罗的监控的最小单位是namespace 也就是 即使同一个namespace 的某一个key变化，也会把整个namespace下的内容推过来 */
 /* 所以建议一个key一个namespace */
 /* 阿波罗页面端口是8070 客户端api端口是8080  */
 namespace apolloconfig {
 	class ApolloConfigClient;
 	class ApolloConfigBase;//配置的基类  具体的实现继承这个
+	//用智能指针做std::unordered_map的key的时候需要实现一个hash函数
+	//用来计算出具体的hash值，如果不实现会导致两个即使内容一样的值算出来的hash值也不一样
+	//必须还得实现相等的比较，不然默认是按指针地址比较的不对的
+	struct shared_pair_hash {
+		template<typename T1, typename T2>
+		std::size_t operator()(const std::shared_ptr<std::pair<T1, T2>>& p)const
+		{
+			return std::hash<T1>{}(p->first) ^ std::hash<T2>{}(p->second);
+		}
+	};
+	struct shared_pair_equal {
+		template<typename T1, typename T2>
+		bool operator () (const std::shared_ptr<std::pair<T1, T2>>& p1, const std::shared_ptr<std::pair<T1, T2>>& p2)const
+		{
+			return p1->first == p2->first && p1->second == p2->second;
+		}
+	};
 	enum HttpCode {
 		HTTP_OK = 200, //watch接口如果有变化会返回这个
-		HTTP_NO_CHANGE=304,//阿波罗watch接口如果配置没变化会返回这个
+		HTTP_NO_CHANGE = 304,//阿波罗watch接口如果配置没变化会返回这个
 	};
 	using HttpClientPtr = std::shared_ptr<SimpleWeb::Client<SimpleWeb::HTTP>>;
 	using Response = std::shared_ptr<SimpleWeb::Client<SimpleWeb::HTTP>::Response>;
-	using ErrorCode = SimpleWeb::error_code;
 	using ParseConfigHandler = std::function<void(std::shared_ptr<std::string>)>;
 	using AppIdClusterPairPtr = std::shared_ptr<std::pair<std::string, std::string>>;
 	class ApolloConfigClient :public LSingleton<ApolloConfigClient>{
@@ -48,27 +68,32 @@ namespace apolloconfig {
 	    ApolloConfigClient();
 		bool Init() { return true; }
 		void Init(const std::string& _ip, uint32_t delaytime=30);
-		void Start();
 		void Join();
 		void Stop();
 		void RegistConfig(const std::string& appId, const std::string& cluster, const std::string& ns, ApolloConfigBase& );
 		static std::string httpurlencode(const std::string& val);
-	private:
+		//获取阿波罗的配置是异步的时候，有些配置是比较重要的，希望能同步获取，或者同步等待异步全部获取完
+		// 比如比赛场的配置 是xml的 文件大概有600多K，我看了下解析需要3s http请求需要大概1s 
+		// 这才是一个配置，以后再加，这个请求、解析时间可能会很长 有必要加一个同步措施 等待配置加载到内存
+		// 所以提供两个版本 一个是直接watch，加载在异步回调里，第二个同步阻塞等待异步加载完成，保证后续操作正常
 		void BeginWatching();
+		void WaitGetAllConfigAndBeginWatching();
+	private:
 
 		void WatchConfig(AppIdClusterPairPtr appIdCluster, HttpClientPtr http = nullptr );
 
-		void WatchCallBack(Response resp, const ErrorCode&err, HttpClientPtr httpclt, AppIdClusterPairPtr appIdCluster);
+		void WatchCallBack(Response resp, const boost::system::error_code&err, HttpClientPtr httpclt, AppIdClusterPairPtr appIdCluster);
 
 		void GetOneConfig(AppIdClusterPairPtr appIdCluster, const  std::string& ns);
 
-		void GetConfigCallBack(Response resp, const ErrorCode&err, HttpClientPtr httpclt  );
+		void GetConfigCallBack(Response resp, const boost::system::error_code&err, HttpClientPtr httpclt  );
 		template<typename Function>
 		void DelayExcute(unsigned int seconds, Function func);
 
 		void DelTimer(uint32_t);
 
 		std::string GetLocalIp()noexcept;
+
 
 		HttpClientPtr MakeHttpClient();
 		std::string								       m_address;
@@ -79,12 +104,12 @@ namespace apolloconfig {
 		//每个key对应的解析handler key 是 appId cluster namespace 
 		std::unordered_map<std::string, ParseConfigHandler > m_configParseHandler;
 		//watch 的时候是以appID和cluster两个维度为准
-		std::unordered_map<AppIdClusterPairPtr , std::vector<std::string> > m_appIdAndCluster;
+		std::unordered_map<AppIdClusterPairPtr , std::vector<std::string>, shared_pair_hash, shared_pair_equal> m_appIdAndCluster;
 		//notifications 是以appid cluster namespace 三个维度确定的 这个值在配置发生变化会变化
 		std::unordered_map<std::string, int64_t>  m_notificationId;//appid.cluster.namespace 2 notification
 		uint32_t     m_delaytime = 30;//网络出现异常的时候  延时调用的时间
 		std::string  m_localIP;
-		// 配置如果出现更改 需要调用具体配置实现的解析，解析成功后更改内存中的配置，注意加锁
+		std::atomic<uint32_t>  m_totalConfigs;
 	};
 	/* namespace最好在创建的时候，指定具体的类型，比如json xml 这样这里传入的namespace要带上.json这样 
 	   比如 commonconfig.json  commonconfig就是namespace 类型为json 
@@ -92,7 +117,6 @@ namespace apolloconfig {
 	class ApolloConfigBase {
 	public:
 		virtual void ParseConfig(std::shared_ptr<std::string>) = 0;//具体的配置只需要实现这一个接口就行，解析具体配置
-		boost::shared_mutex  m_rwlock;//读写锁 更新、读取配置的时候用
 	};
 
 	template<typename FunctionType>
@@ -117,4 +141,13 @@ namespace apolloconfig {
 	}
 }
 #define gApolloConfig apolloconfig::ApolloConfigClient::Instance()
+/*
+实际用的时候
+gApolloConfig.Init("127.0.0.1");
+gApolloConfig.RegistConfig("first_project", "default", "other_test.json", gapollotest);
+gApolloConfig.BeginWatching();//如果启动的时候需要加载完成则需要WaitGetAllConfigAndBeginWatching
+然后对应的配置类实现解析的函数 ParseConfig
+解析成功后交换内存中的数据，注意加锁
+
+*/
 #endif
